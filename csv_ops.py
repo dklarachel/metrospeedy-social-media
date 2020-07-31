ba22f8220f184d6299056af17ed0746986a0796a
Rachel Lin <rachlin232@gmail.com>
2020-07-31 08:38:46 -0400

    loop through list of hashtags and search them

---
+++ csv_ops.py
@@ -0,0 +1,41 @@
+import csv
+
+from random import random
+from random import randint
+
+hashtags = []
+
+def get_hashtags ():
+    with open('hashtags.csv') as hashtags_file:
+        reader = csv.reader(hashtags_file, delimiter=',')
+        row_count = 0
+        for row in reader:
+            if row_count != 0:
+                hashtags.append(row[0])
+            row_count =+ 1
+        return hashtags
+
+def write_hashtag ():
+    fieldnames = []
+    with open('hashtags copy.csv', mode="r") as hashtags_file:
+        reader = csv.DictReader(hashtags_file)
+        row_count = 0
+        for row in reader:
+            if row_count == 0:
+                fieldnames = list(row.keys())
+            row_count += 1
+    with open('hashtags copy.csv', mode="w") as hashtags_file:
+        writer = csv.DictWriter(hashtags_file, fieldnames=fieldnames, lineterminator = '\n')
+        writer.writeheader()
+        writer.writerow({'instagram':'hi'})
+
+
+
+# with open('hashtags.csv', mode='r') as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count != 0:
+#             print(row)
+#         line_count =+ 1
+    
--- instagram.py
+++ instagram.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+from csv_ops import get_hashtags
+
 driver = webdriver.Chrome("chromedriver.exe")
 driver.maximize_window()
 
@@ -13,45 +15,70 @@ driver.get('https://instagram.com')
 username = 'metrospeedy'
 password = 'Summer2017!'
 
-# enter username
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-username_field = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input')
-username_field.click()
-username_field.send_keys(username)
+def login (username, password):
+    # enter username
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+    username_field = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input')
+    username_field.click()
+    username_field.send_keys(username)
 
-# enter password
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
-password_field = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input')
-password_field.click()
-password_field.send_keys(password)
-password_field.send_keys(Keys.RETURN)
+    # enter password
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+    password_field = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input')
+    password_field.click()
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN) # enter to login
 
-# click login button
-# login_button = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button')
-# login_button.click()
+    # click "not now" for notifs
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div/div/div/div[3]/button[2]")))
+    not_now = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
+    # driver.execute_script("arguments[0].click();", not_now)
+    not_now.click()
 
-# click "not now" for notifs
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div/div/div/div[3]/button[2]")))
-not_now = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
-# driver.execute_script("arguments[0].click();", not_now)
-not_now.click()
+# def search_query (search_list):
+#     # enter query into search bar
+#     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div/div/span[1]')))
+#     search_bar = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div/div/span[1]')
+#     search_bar.click()
+#     search_bar_field = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+#     search_bar_field.send_keys('#delivery')
+#     search_bar_field.send_keys(Keys.RETURN)
 
-# enter query into search bar
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div/div/span[1]')))
-search_bar = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div/div/span[1]')
-search_bar.click()
-search_bar_field = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
-search_bar_field.send_keys('#delivery')
-search_bar_field.send_keys(Keys.RETURN)
+#     # click on hashtag that was searched
+#     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div[2]/div/a[1]')))
+#     searched_query_link = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div[2]/div/a[1]')
+#     searched_query_link.click()
+#     # add something to make sure it's the right one?
 
-# click on hashtag that was searched
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div[2]/div/a[1]')))
-searched_query_link = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div[2]/div/a[1]')
-driver.execute_script("arguments[0].click();", searched_query_link)
-# add something to make sure it's the right one?
+#     # get post count for search query
+#     WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/header/div[2]/div[1]/div[2]/span/span')))
+#     post_count = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/header/div[2]/div[1]/div[2]/span/span')
+#     post_count = post_count.text.replace(",", "")
+#     print(post_count)
 
-# get post count for search query
-WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/header/div[2]/div[1]/div[2]/span/span')))
-post_count = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/header/div[2]/div[1]/div[2]/span/span')
-post_count = post_count.text
-print(post_count)
+def search_query (search_list):
+    for x in search_list:
+        # enter query into search bar
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div/div/span[1]')))
+        search_bar = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div/div/span[1]')
+        search_bar.click()
+        search_bar_field = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+        search_bar_field.send_keys(x)
+        search_bar_field.send_keys(Keys.RETURN)
+
+        # click on hashtag that was searched
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div[2]/div/a[1]')))
+        searched_query_link = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div[2]/div/a[1]')
+        searched_query_link.click()
+        # add something to make sure it's the right one?
+
+        # get post count for search query
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/header/div[2]/div[1]/div[2]/span/span')))
+        post_count = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/header/div[2]/div[1]/div[2]/span/span')
+        post_count = post_count.text.replace(",", "")
+        print(post_count)
+
+
+login('metrospeedy', 'Summer2017!')
+search_query(get_hashtags())
+
